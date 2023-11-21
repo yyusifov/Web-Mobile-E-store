fc01d0b748f4d1ad4acf7e78dec77f3ddeba109d
Yadigar Yusifov <yusifovyadigar1@gmail.com>
2023-11-21 23:49:17 +0400

    testing if data is accessed properly

--- src/js/homePage.js
+++ src/js/homePage.js
@@ -4,9 +4,14 @@ fetch("https://dummyjson.com/products").then(response =>{
     }
     return response.json();
 }).then(data =>{
-    
-    
-
+    var jsonString = JSON.stringify(data);
+            
+    // Log the JSON string to the console
+    if(data != null){
+    document.getElementById("img1").src = data.products[0].images[0];
+    alert(data.products[0].title);
+    document.getElementById("productName").textContent = data.products[0].title;
+    }
 }).catch(error => {
     console.error(error);
 })
